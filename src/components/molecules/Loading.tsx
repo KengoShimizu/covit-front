caa8098590ce3b4309b153ca11c6ec4e388e042c
相澤春伽 <aizawadesign13@gmail.com>
2020-09-04 14:40:02 +0900

    [add]CSS

--- src/components/molecules/Loading.tsx
+++ src/components/molecules/Loading.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
 
-/* FIXME ざわちゃんにcssでローディングアニメーション作ってもらう */
 const Loading: React.FC = () => {
   return (
     <div className="loading">
       <style jsx>
         {`
-        .loading{
-          height: 100px;
-          width: 100px;
-          background-color: black;
-          z-index: 1000;
+        .loading,
+        .loading:after {
+          border-radius: 50%;
+          width: 2em;
+          height: 2em;
+        }
+        .loading {
+          // margin: 60px auto;
+          font-size: 14px;
           position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          -webkit-transform: translate(-50%, -50%);
+          z-index: 10000;
+          left: calc(50% - 14px);
+          top: 60px;
+          text-indent: -9999em;
+          //border
+          border-top: .3em solid rgba(255, 255, 255, .5);
+          border-right: .3em solid rgba(255, 255, 255, .5);
+          border-bottom: .3em solid rgba(255, 255, 255, .5);
+          border-left: .3em solid #DF6059;
+          //transform
+          -webkit-transform: translateZ(0);
+          -ms-transform: translateZ(0);
+          transform: translateZ(0);
+          -webkit-animation: load8 1.1s infinite linear;
+          animation: load8 1.1s infinite linear;
+        }
+        @-webkit-keyframes load8 {
+          0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+          }
+          100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+          }
+        }
+        @keyframes load8 {
+          0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+          }
+          100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+          }
         }
       `}
       </style>
