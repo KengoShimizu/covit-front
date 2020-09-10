9c1e1b33f8cfa3d41a9d80256c2554331b78ba95
KengoShimizu <qwerty0308kengo@gmail.com>
2020-09-10 20:17:23 +0900

    send

--- src/components/pages/common/account/Send.tsx
+++ src/components/pages/common/account/Send.tsx
@@ -12,6 +12,7 @@ import Button, { ButtonThemes } from '../../../atoms/Button';
 const Send: React.FC = (props: any) => {
   const [resend, setResend] = useState("");
   const [err, setErr] = useState("");
+  const [sent, setSent] = useState(false);
   const resendPath =
     props.location.state.text === 'ログイン' ?
       'resend_login_email' :
@@ -25,6 +26,7 @@ const Send: React.FC = (props: any) => {
         email: props.location.state.email
       });
       setResend('メールを再送しました。');
+      setSent(true)
       setErr('');
     } catch (error) {
       setErr('エラーが発生しました。もう一度お試しください。');
@@ -64,7 +66,7 @@ const Send: React.FC = (props: any) => {
         <Text theme={[TextThemes.CAPTION]}>{props.location.state.email} <span className="text">宛に認証メールを送信しました。</span></Text>
         <Text theme={[TextThemes.TEXT]}>メール内に記載されているリンクをクリックして{props.location.state.text}を完了してください。</Text>
         <div className="btns-container">
-          <Button theme={[ButtonThemes.NORMAL]} onClick={send} propStyle={{ margin: '10px auto' }}>
+          <Button theme={sent ? [ButtonThemes.SUBNORMAL] : [ButtonThemes.NORMAL]} onClick={sent ? () => {} : send} propStyle={{ margin: '10px auto' }}>
             もう一度メールを送信する
           </Button>
           <Button theme={[ButtonThemes.SUBNORMAL]} propStyle={{ margin: '10px auto' }} onClick={handleClick}>
